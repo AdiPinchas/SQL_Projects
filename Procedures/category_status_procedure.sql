b8fee452a065a7719505d0b1d260aeb65e7f65da
Adi Pinchas <121346615+AdiPinchas@users.noreply.github.com>
2022-12-30 22:23:05 +0200

    Update and rename Category Status procedure.sql to category_status_procedure.sql

--- Procedures/category_status_procedure.sql
+++ Procedures/category_status_procedure.sql
@@ -1,4 +1,14 @@
-alter procedure spCatStatus 
+--Using database-  SQL-Projects/databases/mssql_Sales_DWH_Database.sql 
+
+---This procedure receives a product ID,counts how many products there are in the same category and what is the total price of
+---all products in the same category.
+--If there are more than 100 products and a total price of more than 1000 - the procedure will display Cat Strong
+--If there are more than 40 products and a total price between 480 and 1000, the procedure will display Cat Medium
+--If there are less than 40 products in the category or the category name is empty or NULL (it doesn't matter what the total price is
+--the products) ,the procedure will displa yCat Weak 
+
+
+create procedure spCatStatus 
 @partid int
 as
 begin
